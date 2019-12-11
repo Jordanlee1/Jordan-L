f0e7f15cd78d5e4c6c8047f2b0b519c26960440e
Jordanlee1 <58793090+Jordanlee1@users.noreply.github.com>
2019-12-11 14:40:06 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+var a=document.getElementById("bio");
+
+var b=document.getElementById("text");
+
+function changeImage1(){
+	document.getElementById("optimus-sound").pause();
+	document.getElementById("optimus-sound").currentTime=0;
+	document.getElementById("goku-sound").pause();
+	document.getElementById("goku-sound").currentTime=0;
+	document.getElementById("ironman-sound").pause();
+	document.getElementById("ironman-sound").currentTime=0;
+	document.getElementById("ratchet-sound").pause();
+	document.getElementById("ratchet-sound").currentTime=0;
+	document.getElementById("spongebob-sound").pause();
+	document.getElementById("spongebob-sound").currentTime=0;
+	document.getElementById("samus-sound").play();
+	document.getElementById("main-img").src = "images/samusmainimage.png";
+	a.innerHTML="<h3><strong>SAMUS ARAN:</strong> At a very young age, Samus Aran lost both of her parents on K-2l after an attack from the space pirates and their Leader Ridley. She was then found by a bird-like alien race called the Chozo. They took her back to their home planet Zebes and infused her with Chozo DNA. The Chozo DNA greatly increased her athletic abilitly and gave her a resistance to foriegn environments. On top of that, the Chozo gave Samus a Power Suit and a Gunship. Now she is a galactic bounty hunter that has worked for the Galactic Federation, destroyed the parasites known as Metroids, and defeated the space pirates. She has been a very iconic female video game character and has appeared in other video games like Super Smash Bros.</h3>"
+}
+
+function changeImage2(){
+	document.getElementById("samus-sound").pause();
+	document.getElementById("samus-sound").currentTime=0;
+	document.getElementById("goku-sound").pause();
+	document.getElementById("goku-sound").currentTime=0;
+	document.getElementById("ironman-sound").pause();
+	document.getElementById("ironman-sound").currentTime=0;
+	document.getElementById("ratchet-sound").pause();
+	document.getElementById("ratchet-sound").currentTime=0;
+	document.getElementById("spongebob-sound").pause();
+	document.getElementById("spongebob-sound").currentTime=0;
+	document.getElementById("optimus-sound").play();
+	document.getElementById("main-img").src = "images/optimusprimemainimage.png";
+	a.innerHTML="<h3><strong>OPTIMUS PRIME:</strong> After taking heavy damage from Megatron as Orion Pax, Alpha Trion took his damgaed body and remade it into the Autobot Leader we know today. He bears the Matrix of Leadership and has lead the war for Cybertron against the Decepticons for eight million stellar cycles. Optimus and the autobots eventually left cybertron to find a new, safer home when they were followed by the Deceptions. The battle caused both of the factions to land on Earth. Now Optimus leads the Autobots to defends the planet from the Decepticons. Over the years, Prime remained one of the most reconizable transformers and has had many appearences and changes.</h3>"
+}
+
+function changeImage3(){
+	document.getElementById("optimus-sound").pause();
+	document.getElementById("optimus-sound").currentTime=0;
+	document.getElementById("samus-sound").pause();
+	document.getElementById("samus-sound").currentTime=0;
+	document.getElementById("ironman-sound").pause();
+	document.getElementById("ironman-sound").currentTime=0;
+	document.getElementById("ratchet-sound").pause();
+	document.getElementById("ratchet-sound").currentTime=0;
+	document.getElementById("spongebob-sound").pause();
+	document.getElementById("spongebob-sound").currentTime=0;
+	document.getElementById("goku-sound").play();
+	document.getElementById("main-img").src = "images/gokumainimage.png";
+	a.innerHTML="<h3><strong>Goku:</strong> Young Kakarott was sent to planet Earth to destroy it. When he landed on earth, he was adopted by Grandpa Gohan, who taught him martial arts. While he was with Grandpa Gohan, he recieved a serious head injury that made him forget all about his mission to destroy the Earth and instead he protects the planet under his new name, Goku. Sadly while goku was a child, he accidently killed Gohan during his rage as a great ape and only found out once he fought Vegeta in his great ape form. Over the years, Goku continued to grow stronger and protected the earth from serious threats like King Piccolo, Prince Vegeta, Cell, and Majin Buu and many more to come. He has even defeated emperor Frieza. Goku loves a good and fair fight and has made many friends and enemies. He has been seen in lots of media and is easily the most reconizable anime and manga character there is today.</h3>"
+}
+
+function changeImage4(){
+	document.getElementById("optimus-sound").pause();
+	document.getElementById("optimus-sound").currentTime=0;
+	document.getElementById("goku-sound").pause();
+	document.getElementById("goku-sound").currentTime=0;
+	document.getElementById("samus-sound").pause();
+	document.getElementById("samus-sound").currentTime=0;
+	document.getElementById("ratchet-sound").pause();
+	document.getElementById("ratchet-sound").currentTime=0;
+	document.getElementById("spongebob-sound").pause();
+	document.getElementById("spongebob-sound").currentTime=0;
+	document.getElementById("ironman-sound").play();
+	document.getElementById("main-img").src = "images/ironmanmainimage.png";
+	a.innerHTML="<h3><strong>Iron Man:</strong> Tony Stark was a very wealthy and intelligent man. When he was 21, he ran his father's company and made it into the huge success it is today. However Stark's luck ran out when he was testing technololgy in Vietnam. A trap had activated and he was hit by a piece of shrapnel. He was then kidnapped and forced to build a weapon of mass destruction for his captors. He of course doesn't do that and instead builds a mechanical suit of armor that helped him live and helped him escape his kidnappers. After he escaped, he improved the suit and added a few weapons, gadgets, and other devices to it. With jis new and improved suit, he protected the world as Iron Man. He joined the avengers and helped protect the planet from even bigger threats. He is a very well-known comic book and marvel character (If not the best marvel character) Love you 3000.</h3>"
+}
+
+function changeImage5(){
+	document.getElementById("optimus-sound").pause();
+	document.getElementById("optimus-sound").currentTime=0;
+	document.getElementById("goku-sound").pause();
+	document.getElementById("goku-sound").currentTime=0;
+	document.getElementById("ironman-sound").pause();
+	document.getElementById("ironman-sound").currentTime=0;
+	document.getElementById("samus-sound").pause();
+	document.getElementById("samus-sound").currentTime=0;
+	document.getElementById("spongebob-sound").pause();
+	document.getElementById("spongebob-sound").currentTime=0;
+	document.getElementById("ratchet-sound").play();
+	document.getElementById("main-img").src = "images/ratchetmainimage.png";
+	a.innerHTML="<h3><strong>Ratchet:</strong> Ratchet is a Lombax that was born on the Planet Fastoon in the Polaris Galaxy. His father later sent him to planet Veldin in Solana Galaxy to protect emperor Tachyon. During his time on Veldin, Ratchet had the desire to build his very own spaceship and travel the galaxy. After he built his ship, he meets a robot fugitive named Clank. He then lives his dream and travels the galaxy with Clank. Ratchet is a very popular Playstation character that eventually got his own movie.</h3>"
+}
+
+function changeImage6(){
+	document.getElementById("optimus-sound").pause();
+	document.getElementById("optimus-sound").currentTime=0;
+	document.getElementById("goku-sound").pause();
+	document.getElementById("goku-sound").currentTime=0;
+	document.getElementById("ironman-sound").pause();
+	document.getElementById("ironman-sound").currentTime=0;
+	document.getElementById("ratchet-sound").pause();
+	document.getElementById("ratchet-sound").currentTime=0;
+	document.getElementById("samus-sound").pause();
+	document.getElementById("samus-sound").currentTime=0;
+	document.getElementById("spongebob-sound").play();
+	document.getElementById("main-img").src = "images/spongebobmainimage.png";
+	a.innerHTML="<h3><strong>Spongebob:</strong> There's really no need for a bio about spongebob. He is a well-known nickelodean character. He has had lots of movies, video games, comics, and toys made to honor him. He was created by a marine biologist named Stephen Hillenburg. The original title for spongebob was supposed to be SpongeBoy Ahoy but they later renamed the title into the one we all know and love today. Unfortunately on November 26, 2018, Stephen Hillenburg died of heart failure from a diease. He will be missed and will forever be known for his amazing creation. Thank you Stephen Hillenburg.</h3>"
+}
